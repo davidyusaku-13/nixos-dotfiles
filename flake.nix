e3d9ab4054e5521fdcaae381953a7d4b48b4b956
David Yusaku <davidyusaku13@gmail.com>
2026-07-12 12:57:40 +0700

    fix: capture inputs in outputs function in flake.nix

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, stylix, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, stylix, ... }: {
     nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit self inputs; };
