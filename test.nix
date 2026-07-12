208af68363cbf3a1859fd65188d62f4288da44ca
David Yusaku <davidyusaku13@gmail.com>
2026-07-12 09:56:25 +0700

    chore: remove orphaned test.nix scratchpad

--- test.nix
+++
@@ -1,8 +0,0 @@
-
-let
-  apps = [ "hypr" "waybar" "foot" "nvim" ];
-in
-  builtins.listToAttrs (map (app: {
-    name = ".config/${app}";
-    value = { source = ./config + "/${app}"; force = true; };
-  }) apps)
